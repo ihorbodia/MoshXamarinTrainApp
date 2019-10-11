4eee7df9d6137bb156673075be56c9c3c9804a4c
Ihor <ibodia@sii.pl>
2019-10-11 10:43:43 +0200

    Initial commit.

---
+++ TrainXamarinApp/TrainXamarinApp/GreetPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrainXamarinApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
